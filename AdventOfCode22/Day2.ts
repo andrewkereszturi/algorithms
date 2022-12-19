68e745a961923bf970b69bdcab4fa7f9ea4b4d63
andrewkereszturi <andrew.kereszturi@gmail.com>
2022-12-19 10:11:08 -0800

    Day2 part 2

--- AdventOfCode22/Day2.ts
+++ AdventOfCode22/Day2.ts
@@ -4,38 +4,38 @@ const run = async () => {
   const rawLines = readFileSync("Day2-input.txt", "utf8");
   const lines = rawLines.split("\n");
 
+  const player1Wins = (a: any, b: any) => {
+    if (a === "A" && b === "Z") {
+        return true;
+    } else if (a === "B" && b === "X") {
+        return true;
+    } else if (a === "C" && b === "Y") {
+        return true;
+    } else {
+        return false;
+    } 
+  }
+
+  const player2Wins = (a: any, b: any) => {
+    if (b === "X" && a === "C") {
+         return true;
+    } else if (b === "Y" && a === "A") {
+         return true;
+    } else if (b === "Z" && a === "B") {
+        return true;
+    } else {
+       return false;
+      } 
+  }
+
+  interface pointsType {  
+    [key: string]: number;
+  } 
+  
+  const points : pointsType = { "A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3 };  
+
   const rockPaperScissors = (strategy: any) => {
 
-    interface pointsType {  
-        [key: string]: number;
-    } 
-      
-    const points : pointsType = { "A": 1, "B": 2, "C": 3, "X": 1, "Y": 2, "Z": 3 };  
-
-    const player1Wins = (a: any, b: any) => {
-        if (a === "A" && b === "Z") {
-            return true;
-        } else if (a === "B" && b === "X") {
-            return true;
-        } else if (a === "C" && b === "Y") {
-            return true;
-        } else {
-            return false;
-        } 
-    }
-
-    const player2Wins = (a: any, b: any) => {
-        if (b === "X" && a === "C") {
-            return true;
-        } else if (b === "Y" && a === "A") {
-            return true;
-        } else if (b === "Z" && a === "B") {
-            return true;
-        } else {
-            return false;
-        } 
-    }
-       
     let totalPoints1 = 0;
     let totalPoints2 = 0;
 
@@ -51,10 +51,39 @@ const run = async () => {
             totalPoints1 += points[strategy[i][0]]
         } 
     }
-     console.log(totalPoints1, totalPoints2)
+     console.log("Part One: ", totalPoints1, totalPoints2)
   }
 
-  console.log("TOP POINTS: ", rockPaperScissors(lines));
+  const partTwo = (strategy: any) => {
+
+    let totalPoints = 0;
+
+    for (let i = 0; i < strategy.length; i++) {
+        if (strategy[i][0] === "A" && strategy[i][2] === "X") {
+             totalPoints+= 3;
+        } else if (strategy[i][0] === "A" && strategy[i][2] === "Y") {
+            totalPoints+= 4;
+        } else if (strategy[i][0] === "A" && strategy[i][2] === "Z") {
+            totalPoints+= 8;
+        } else if (strategy[i][0] === "B" && strategy[i][2] === "X") {
+            totalPoints+= 1;
+        } else if (strategy[i][0] === "B" && strategy[i][2] === "Y") {
+            totalPoints+= 5;
+        } else if (strategy[i][0] === "B" && strategy[i][2] === "Z") {
+            totalPoints+= 9;
+        } else if (strategy[i][0] === "C" && strategy[i][2] === "X") {
+            totalPoints+= 2;
+        } else if (strategy[i][0] === "C" && strategy[i][2] === "Y") {
+            totalPoints+= 6;
+        } else if (strategy[i][0] === "C" && strategy[i][2] === "Z") {
+            totalPoints+= 7;
+        } 
+    }
+     console.log("Part Two: ", totalPoints)
+  }
+
+  rockPaperScissors(lines);
+  partTwo(lines);
 
 };
 
