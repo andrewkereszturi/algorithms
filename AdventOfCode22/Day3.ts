a29db4e7721bb782a34cdb4fff1c5797fdb327f3
andrewkereszturi <andrew.kereszturi@gmail.com>
2022-12-19 12:59:53 -0800

    Day3 part 1 and 2

---
+++ AdventOfCode22/Day3.ts
@@ -0,0 +1,63 @@
+import { readFileSync } from 'fs';
+
+const run = async () => {
+  const rawLines = readFileSync("Day3-input.txt", "utf8");
+  const lines = rawLines.split("\n");
+
+  interface pointsType {  
+    [key: string]: number;
+  } 
+  
+  const points : pointsType = { "a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8, "i": 9, "j": 10, "k": 11, "l": 12, "m": 13, "n": 14, "o": 15, "p": 16, "q": 17, "r": 18, "s": 19, "t": 20, "u": 21, "v": 22, "w": 23, "x": 24, "y": 25, "z": 26, "A": 27, "B": 28, "C": 29, "D": 30, "E": 31, "F": 32, "G": 33, "H": 34, "I": 35, "J": 36, "K": 37, "L": 38, "M": 39, "N": 40, "O": 41, "P": 42, "Q": 43, "R": 44, "S": 45, "T": 46, "U": 47, "V": 48, "W": 49, "X": 50, "Y": 51, "Z": 52}; 
+
+  let rucksackPoints = (rucksack: any) => { 
+
+    let totalPoints = 0;
+
+    for (let i = 0; i < rucksack.length; i++) {
+
+        let firstHalf = rucksack[i].substring(0, rucksack[i].length / 2)
+        let secondHalf = rucksack[i].substring(rucksack[i].length / 2, rucksack[i].length)
+
+        let secondSplit = secondHalf.split("");
+
+        for (let j = 0; j < firstHalf.length; j++) {
+            if (secondSplit.includes(firstHalf[j])) {
+                let point = points[firstHalf[j]]
+                totalPoints += point;
+                break;
+            }
+        }
+
+    }
+        return totalPoints;
+  }
+
+  let rucksackGroupPoints = (rucksack: any) => { 
+
+    let totalPoints = 0;
+
+    for (let i = 0; i < rucksack.length-2; i += 3) {
+
+        let second = rucksack[i+1].split("")
+        let third = rucksack[i+2].split("")
+
+        for (let j = 0; j < rucksack[i].length; j++) {
+            if (second.includes(rucksack[i][j])) {
+                if(third.includes(rucksack[i][j])) {
+                    let point = points[rucksack[i][j]]
+                    totalPoints += point;
+                    break;
+                }
+            }
+        }
+    }
+        return totalPoints;
+  }
+
+  console.log("Rucksack Total Points", rucksackPoints(lines));
+  console.log("Rucksack Group Points", rucksackGroupPoints(lines));
+
+};
+
+run();
