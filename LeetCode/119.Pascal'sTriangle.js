6bf882c0391e6c4a1daac6b06da4a61ae0d17e41
andrewkereszturi <andrew.kereszturi@gmail.com>
2023-04-20 16:21:27 -0700

    Pascal's Triangle Recursive without helper

--- LeetCode/119.Pascal'sTriangle.js
+++ LeetCode/119.Pascal'sTriangle.js
@@ -24,36 +24,61 @@ var generate = function(numRows) {
     return triangle;
 };
 
+// Recursive Solution with helper
+// var generateTriangle = function(numRows, triangle) {
+
+//     if (numRows === 2) {
+//         return triangle;
+//     }
+
+//     let newRow = [1,1];
+//     let lastRow = triangle[triangle.length-1];
+
+//     for (let i = 0; i < lastRow.length-1; i++) {
+//         newRow.splice(i+1, 0, lastRow[i] + lastRow[i+1])
+//     }    
+
+//     triangle.push(newRow)
+//     return generateTriangle(numRows - 1, triangle)
+// };
+
+// var generate = function(numRows) {
+
+//     let triangle = [[1], [1,1]];
+
+//     if (numRows === 1) {
+//         return [[1]];
+//     } else if (numRows === 2) {
+//         return triangle;
+//     }
+
+//     return generateTriangle(numRows, triangle)
+// };
+
 // Recursive Solution
-var generateTriangle = function(numRows, triangle) {
-
-    if (numRows === 2) {
-        return triangle;
-    }
-
-    let newRow = [1,1];
-    let lastRow = triangle[triangle.length-1];
-
-    for (let i = 0; i < lastRow.length-1; i++) {
-        newRow.splice(i+1, 0, lastRow[i] + lastRow[i+1])
-    }    
-
-    triangle.push(newRow)
-    return generateTriangle(numRows - 1, triangle)
-};
 
 var generate = function(numRows) {
 
-    let triangle = [[1], [1,1]];
-
     if (numRows === 1) {
         return [[1]];
-    } else if (numRows === 2) {
-        return triangle;
     }
 
-    return generateTriangle(numRows, triangle)
-};
+    const previousRows = generate(numRows - 1);
+    const previousRow = previousRows[previousRows.length - 1];
+
+    const row = [1];
+
+    for (let i = 1; i < numRows-1; i++) {
+        row.push(previousRow[i-1] + previousRow[i]) 
+    }
+
+    row.push(1);
+
+    return [
+        ...previousRows,
+        row
+    ]
+}
 
 let result = generate(1);
 console.log(result)
