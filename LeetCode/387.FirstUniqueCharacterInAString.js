1e4be860fe03b443ce5653579d520b09ce4ab70d
andrewkereszturi <andrew.kereszturi@gmail.com>
2023-05-04 16:21:58 -0700

    LeetCode FirstUniqueCharacterInAString

---
+++ LeetCode/387.FirstUniqueCharacterInAString.js
@@ -0,0 +1,24 @@
+var firstUniqChar = function(s) {
+    let seen = {}
+
+    for(let i = 0; i < s.length; i++) {
+        seen[s[i]] = (seen[s[i]]+1) || 1 ;
+    }
+
+    for(letter in seen ) {
+        if (seen[letter] === 1) {
+            return s.indexOf(letter);
+        }
+    }
+    
+    return -1;
+};
+
+// 0
+console.log(firstUniqChar("leetcode"));
+
+// 2
+console.log(firstUniqChar("loveleetcode"));
+
+// -1
+console.log(firstUniqChar("aabb"));
