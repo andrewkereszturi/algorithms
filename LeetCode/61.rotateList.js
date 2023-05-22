ab90e2a459aeaad019c30e9a0e69a31ca3d1d401
andrewkereszturi <andrew.kereszturi@gmail.com>
2023-05-21 22:11:27 -0700

    Leetcode rotateList recursive solution cleaned up - stack overflow

--- LeetCode/61.rotateList.js
+++ LeetCode/61.rotateList.js
@@ -1,3 +1,31 @@
+// const removeLastNode = (head) => {
+//     if (head === null || head.next === null){
+//         return null;
+//     }
+//     let newTail = head;
+//     while (newTail.next.next !== null) {
+//         newTail = newTail.next;
+//     }
+//     newTail.next = null;
+//     return head;
+// }
+ 
+// var rotateRight = function(head, k) {  
+//     if (!head || k === 0) {
+//         return head;
+//     } 
+//     let l1 = head
+//     while(l1.next !== null) {
+//         l1 = l1.next
+//     }
+//     const oneLess = removeLastNode(head);
+
+//     l1.next = oneLess
+//     return rotateRight(l1, --k)
+
+// };
+
+// Cleaned up Solution 
 const removeLastNode = (head) => {
     if (head === null || head.next === null){
         return null;
@@ -9,18 +37,31 @@ const removeLastNode = (head) => {
     newTail.next = null;
     return head;
 }
+
+const getLastNode = (head) => {
+    if (head === null || head.next === null){
+        return null;
+    }
+    let newTail = head;
+    while (newTail.next.next !== null) {
+        newTail = newTail.next;
+    }
+    return newTail.next;
+    
+}
  
 var rotateRight = function(head, k) {  
-    if (!head || k === 0) {
-        return head;
+
+    if(!head || !head.next) {
+        return head
     } 
+
     let l1 = head
-    while(l1.next !== null) {
-        l1 = l1.next
+    for(let i = 0; i < k; i++) {
+        let newHead = getLastNode(l1)
+        let tail = removeLastNode(l1);
+        newHead.next = tail
+        l1 = newHead
     }
-    const oneLess = removeLastNode(head);
-
-    l1.next = oneLess
-    return rotateRight(l1, --k)
-
+    return l1
 };
