605f1d300b3d80f1d9a726a1fb6470e9f24a1997
andrewkereszturi <andrew.kereszturi@gmail.com>
2023-05-25 14:06:55 -0700

    Leetcode rotateArray working solution

--- LeetCode/189.rotateArray.js
+++ LeetCode/189.rotateArray.js
@@ -1,20 +1,20 @@
-// Initial solution, doesn't pass largest test case
 var rotate = function(nums, k) {
 
-    // for(let i = 0; i < k % nums.length; i++) {
-    //     let last = nums.splice(nums.length-1, 1)
-    //     nums.unshift(last)
-    // }
+    if(nums.length === 1) {
+        return nums
+    }
+
     let arr = [...nums]
+
+    if (k > nums.length) {
+        k = k % nums.length
+    }
+
     for(let i = 0; i < nums.length; i++) {
         if(i - k >= 0) {
           nums[i] = arr[i - k]
         } else {
-
-        nums[i] = arr[nums.length - (Math.abs(i-k) % nums.length)]
-
+          nums[i] = arr[nums.length + (i - k)]
         }
     }
-
-    return nums
 };
