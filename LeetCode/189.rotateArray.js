4615dae6de8e531a6528891baf80438b4c3f22c8
andrewkereszturi <andrew.kereszturi@gmail.com>
2023-05-24 20:58:50 -0700

    Leetcode rotateArray start O(n) solution attempt

--- LeetCode/189.rotateArray.js
+++ LeetCode/189.rotateArray.js
@@ -1,9 +1,19 @@
 // Initial solution, doesn't pass largest test case
 var rotate = function(nums, k) {
 
-    for(let i = 0; i < k % nums.length; i++) {
-        let last = nums.splice(nums.length-1, 1)
-        nums.unshift(last)
+    // for(let i = 0; i < k % nums.length; i++) {
+    //     let last = nums.splice(nums.length-1, 1)
+    //     nums.unshift(last)
+    // }
+    let arr = [...nums]
+    for(let i = 0; i < nums.length; i++) {
+        if(i - k >= 0) {
+          nums[i] = arr[i - k]
+        } else {
+
+        nums[i] = arr[nums.length - (Math.abs(i-k) % nums.length)]
+
+        }
     }
 
     return nums
