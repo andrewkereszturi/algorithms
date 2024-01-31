c4b3b5bde9c4a402777dcba0683fadc8111ade05
andrewkereszturi <andrew.kereszturi@gmail.com>
2024-01-30 21:19:21 -0800

    Leetcode Missing Number

---
+++ LeetCode/268.MissingNumber.js
@@ -0,0 +1,9 @@
+var missingNumber = function(nums) {
+    const set = new Set(nums)
+    for (let i = 0; i < nums.length; i++) {
+        if(!set.has(i)) {
+            return i
+        }
+    }
+    return set.size
+};
