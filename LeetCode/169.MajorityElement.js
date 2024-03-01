dae72502935cc7dbf82d48dcfaa8373a8b964c99
andrewkereszturi <andrew.kereszturi@gmail.com>
2024-03-01 15:37:28 -0800

    Leetcode Majority Element alternate solution

--- LeetCode/169.MajorityElement.js
+++ LeetCode/169.MajorityElement.js
@@ -13,3 +13,17 @@ console.log(majorityElement([3,2,3]))
 
 // 2
 console.log(majorityElement([2,2,1,1,1,2,2]))
+
+// var majorityElement = function(nums) {
+//     let map = {}
+
+//     for (let i = 0; i < nums.length; i++) {
+//         if (!map[nums[i]]) {
+//             map[nums[i]] = 1
+//         } else {
+//             map[nums[i]]++
+//         }
+//     }
+    
+//     return Object.keys(map).reduce((a, b) => map[a] > map[b] ? a : b)
+// };
