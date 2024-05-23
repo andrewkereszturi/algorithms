9400b32d3de3bed2704d1600b942d0d5196250b0
andrewkereszturi <andrew.kereszturi@gmail.com>
2024-05-22 19:56:50 -0700

    Leetcode Relative Sort Array

---
+++ LeetCode/1122.RelativeSortArray.js
@@ -0,0 +1,9 @@
+var relativeSortArray = function(arr1, arr2) {
+    const map = new Map();
+    
+    for (let i = 0 ; i < arr2.length; i++) {
+        map.set(arr2[i], i);
+    }
+    
+    return arr1.sort((a,b) => (!map.has(a) ? arr1.length + a : map.get(a)) - (!map.has(b) ? arr1.length + b : map.get(b)));
+};
